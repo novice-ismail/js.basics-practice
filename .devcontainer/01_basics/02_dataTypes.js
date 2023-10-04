7cb7464a70753dcef62d9c190101ee2d2e007fcd
novice-ismail <142286494+novice-ismail@users.noreply.github.com>
2023-10-04 04:14:55 +0000

    variables

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3); // code readability should be high
+   
+    console.log("Ismail")
+
+
+    let name = "tony"
+    let age = 18
+    let isLoggedIn = false
+
+
+    // number =>2 to power 53
+    // bigint(trading or stockmarket)
+    // string => ""
+    //boolean => true/false
+    // null => standalone value
+    // undefined => value till not assigned
+    // symbol => unique
+
+
+    // object
+
+    console.log(typeof null); //object
+    console.log(typeof undefined); // undefined
