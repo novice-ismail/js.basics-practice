a69c78e166e9b788833bdadfdbd925f295e3b4ba
novice-ismail <142286494+novice-ismail@users.noreply.github.com>
2023-10-04 05:16:40 +0000

    Opertions

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "ismail"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // ""=> false
@@ -24,5 +24,50 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log( typeof StringNumber);
+// console.log(StringNumber);
+// console.log( typeof StringNumber);
+
+// **************************** Operations ***********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " tony"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+ "2");
+// console.log("1" + 2 + 2);
+// console.log(1 +2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let x = 3
+const y = x++
+
+console.log(`x:${x},y:${y}`);
+let a = 3
+const b = ++a
+
+console.log(`x:${a},y:${b}`);
