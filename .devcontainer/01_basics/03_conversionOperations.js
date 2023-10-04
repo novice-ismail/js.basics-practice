452c8ceec9cd076b772a6f596eaa1272caa1d624
novice-ismail <142286494+novice-ismail@users.noreply.github.com>
2023-10-04 04:49:32 +0000

    conversion of dataTypes

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score) );
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "ismail"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// ""=> false
+//" ismail" => true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log( typeof StringNumber);
