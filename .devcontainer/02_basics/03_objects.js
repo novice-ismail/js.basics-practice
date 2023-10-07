7f1cc865b33175d297c9e98cfa6c3ed9e8405333
novice-ismail <142286494+novice-ismail@users.noreply.github.com>
2023-10-07 05:02:59 +0000

    Objects

---
+++ .devcontainer/02_basics/03_objects.js
@@ -0,0 +1,41 @@
+// singleton
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Fahad",
+    "full name":"Muhammed Fahad",
+   [mySym]: "mykey1",
+    age: 18,
+    location: "Bengaluru",
+    email: "fahad@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(  JsUser[mySym]);
+
+JsUser.email = "fahad@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "fahad@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function () {
+    console.log("Hello JS user");
+    
+}
+
+JsUser.greetingTwo = function () {
+    console.log(`Hello JS user,${this.name}`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
