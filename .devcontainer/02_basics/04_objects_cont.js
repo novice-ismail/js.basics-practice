7b4e9f50f77094826f2a15bdbe250ea57cf7c839
novice-ismail <142286494+novice-ismail@users.noreply.github.com>
2023-10-07 06:30:15 +0000

    object destructure

--- .devcontainer/02_basics/04_objects_cont.js
+++ .devcontainer/02_basics/04_objects_cont.js
@@ -45,9 +45,20 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseinstructor: "hitesh"
+}
+
+const {courseinstructor: instructor} = course
+// console.log(courseinstructor);
+console.log(instructor);
